dc695edd828fd0ec5a573a530ccc44eac1e87a05
JAIME VALLEJO <jaimed.vallejo@gmail.com>
2022-10-21 20:31:20 -0500

    added di

---
+++ app/src/main/java/co/com/nubank/mobile/challenge/di/component/AppComponent.kt
@@ -0,0 +1,27 @@
+package co.com.nubank.mobile.challenge.di.component
+
+import android.content.Context
+import co.com.nubank.mobile.challenge.MainApplication
+import co.com.nubank.mobile.challenge.di.modules.NetworkModule
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjectionModule
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+import javax.inject.Singleton
+
+@Singleton
+@Component(
+    modules = [
+        AndroidInjectionModule::class,
+        NetworkModule::class]
+)
+interface AppComponent : AndroidInjector<DaggerApplication> {
+
+    @Component.Factory
+    interface Factory {
+        fun create(@BindsInstance applicationContext: Context): AppComponent
+    }
+
+    fun inject(app: MainApplication)
+}
